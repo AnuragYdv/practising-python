0a32a05c9cb2591ba60ad05b638defb01c5fa230
Anurag <anurag@jamfmanages-MacBook-Pro.local>
2024-01-27 15:35:08 +0530

    inserting elements in a Binary Tree

--- BinaryTree/BinaryTreeImplementation.py
+++ BinaryTree/BinaryTreeImplementation.py
@@ -1,18 +1,44 @@
 class Node:
-    def __init__(self,data):
-        self.data=data
-        self.leftChild=None
-        self.rightChild=None
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
 
-    def PrintTree(self):
-        print(self.data)
+    def insert(self, data):
+        if data < self.data:
+            if self.leftChild:
+                self.leftChild.insert(data)
+            else:
+                self.leftChild = Node(data)
+                return
+        else:
+            if self.rightChild:
+                self.rightChild.insert(data)
+            else:
+                self.rightChild = Node(data)
+                return
 
-class Driverclass:
-    if __name__=='__main__':
-        node_instance=Node(7)
-        node_instance.leftChild=Node(10)
-        node_instance.rightChild=Node(15)
-        print(node_instance.leftChild.PrintTree())
+    def print_tree(self):
+        if self.leftChild:
+            self.leftChild.print_tree()
+        print( self.data),
+        if self.rightChild:
+            self.rightChild.print_tree()
 
 
 
+class DriverClass:
+    if __name__ == '__main__':
+        root=Node(100)
+        root.insert(10)
+        root.insert(20)
+        root.insert(30)
+        root.insert(40)
+        root.insert(50)
+        root.insert(60)
+        root.insert(110)
+        root.insert(120)
+        root.insert(130)
+        root.insert(125)
+        root.insert(115)
+        print(root.print_tree())
